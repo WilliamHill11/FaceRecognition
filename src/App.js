5d21e9734a7a0780bf854ff3ff7ef5c32e68c149
william hill <williamhill0831@Hotmail.com>
2023-02-06 01:58:10 -0500

    Created the front page with cool tilt/particle npm packages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/Navigation/navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import ParticlesBg from 'particles-bg'
 
 class App extends Component {
   render() {
     return(
       <div className="App">
+        <ParticlesBg type="cobweb" bg={true} />
         <Navigation />
-        {/* <Logo />
+        <Logo />
+        <Rank />
         <ImageLinkForm />
-        <FaceRecognition /> */}
+        {/* <FaceRecognition />  */}
       </div>
     );
   }
